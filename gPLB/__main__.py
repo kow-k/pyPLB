99f5a8da2cbcdc4145c64a58da7116c02d768a52
Kow Kuroda <kow.kuroda@gmail.com>
2024-10-23 15:57:53 +0900

    Update __main__.py

--- gPLB/__main__.py
+++ gPLB/__main__.py
@@ -277,13 +277,17 @@ for i, link in enumerate(M.links):
 ## adding link source z-scores to M
 if verbose:
     print(f"##Link_sources")
-averages_by_rank = calc_averages_by_rank (M.link_sources) # returns dictionary
-stdevs_by_rank   = calc_stdevs_by_rank (M.link_sources) # returns dictionary
+Link_sources = M.link_sources
+averages_by_rank = calc_averages_by_rank (Link_sources) # returns dictionary
+stdevs_by_rank   = calc_stdevs_by_rank (Link_sources) # returns dictionary
+medians_by_rank  = calc_medians_by_rank (Link_sources) # returns dictionary
+MADs_by_rank     = calc_MADs_by_rank (Link_sources) # returns dictionary
+
 source_zscores = {}
-for i, link_source in enumerate(M.link_sources):
+for i, link_source in enumerate(Link_sources):
+    value  = Link_sources[link_source]
     rank   = get_rank_of_list (link_source)
-    value  = M.link_sources[link_source]
-    zscore = calc_zscore (value, averages_by_rank[rank], stdevs_by_rank[rank])
+    zscore = calc_zscore (value, averages_by_rank[rank], stdevs_by_rank[rank], medians_by_rank[rank], MADs_by_rank[rank], robust = True)
     source_zscores[link_source] = zscore
     if verbose:
         print(f"#link_source {i:3d}: {link_source} has {value} offspring(s) [{source_zscores[link_source]:.6f} at rank {rank}]")
@@ -293,24 +297,6 @@ M.source_zscores.update(source_zscores)
 if verbose:
     print(f"M.source_zscores: {M.source_zscores}")
 
-## checking link target z-scores to M
-if verbose:
-    print(f"##Link_targets")
-averages_by_rank = calc_averages_by_rank (M.link_targets) # returns dictionary
-stdevs_by_rank   = calc_stdevs_by_rank (M.link_targets) # returns dictionary
-target_zscores = {}
-for i, link_target in enumerate(M.link_targets):
-    rank   = get_rank_of_list (link_target)
-    value  = M.link_targets[link_target]
-    zscore = calc_zscore (value, averages_by_rank[rank], stdevs_by_rank[rank])
-    target_zscores[link_target] = zscore
-    if verbose:
-        print(f"#link_target {i:3d}: {link_target} has {value} parent(s) [{target_zscores[link_target]:.6f} at rank {rank}]")
-## attach target_zscores to M
-#M.target_zscores = target_zscores
-M.target_zscores.update(target_zscores)
-if verbose:
-    print(f"M.target_zscores: {M.target_zscores}")
 
 ## draw diagram
 if draw_diagrams:
